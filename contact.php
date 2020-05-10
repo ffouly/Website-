d387b9c84caf0ff2cfe9d9330c699e9e3ca5a568
ffouly <57575804+ffouly@users.noreply.github.com>
2020-05-10 16:31:45 -0400

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,23 +1,49 @@
 	<?php
-		if(!empty($_POST['submit'])) {
+				
+
+				$errors = '';
+				$myemail = 'farah.fouly@hotmail.com';//<-----Put Your email address here.
+				if(empty($_POST['firstname'])  || 
+				   empty($_POST['lastname'])  || 
+				   empty($_POST['email']) || 
+				   empty($_POST['message']))
+				{
+				    $errors .= "\n Error: all fields are required";
+				}
+
 				$fname = $_POST['firstname'];
 				$lname = $_POST['lastname'];
 				$email = $_POST['email'];
 				$message = $_POST['message'];
-				$formcontent="From: $fname $lname \n Message: $message";
-				$recipient = "farah.fouly@hotmail.com";
-				$subject = $_POST['subject'];
-				$mailheader = "From: $fname $lname \r\n";
 
-				mail($recipient, $subject, $formcontent, $mailheader);
-
-				if(mail($recipient, $subject, $formcontent, $mailheader)) {
-				    $message = "Your contact information is received successfully.";
-				    $type = "success";
+				if (!preg_match(
+				"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+				$email))
+				{
+				    $errors .= "\n Error: Invalid email address";
 				}
-				else echo '<p> There is an error. Please try again.</p>';
-			} 
-			require_once "contact.php";
+
+				if( empty($errors)){
 				
-			echo "Thank You!" . " -" . "<a href='Contact.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+				$formcontent='From: $fname $lname \n Message: $message';
+				$recipient = 'farah.fouly@hotmail.com';
+				$subject = $_POST['subject'];
+				$mailheader = 'From: $fname $lname \r\n';
+
+				$mail_Sent = mail($recipient, $subject, $formcontent, $mailheader);
+
+				if ($mail_sent == true){ ?>
+			        <script language="javascript" type="text/javascript">
+			        alert('Thank you for the message. We will contact you shortly.');
+			        window.location = 'Contact.html';
+			        </script>
+			    <?php } else { ?>
+			    <script language="javascript" type="text/javascript">
+			        alert('Message not sent. Please, notify the site administrator farah.fouly@hotmail.com');
+			        window.location = 'Contact.html';
+			    </script>
+			    <?php
+			    }
+			
+			}
 		?>
