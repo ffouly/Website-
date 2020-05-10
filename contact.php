d296088265a9b9251833bd75e2ccf7ec87cc8205
ffouly <57575804+ffouly@users.noreply.github.com>
2020-05-09 21:46:26 -0400

    Add files via upload

--- contact.php
+++ contact.php
@@ -9,11 +9,14 @@
 				$subject = $_POST['subject'];
 				$mailheader = "From: $fname $lname \r\n";
 
+				mail($recipient, $subject, $formcontent, $mailheader);
+
 				if(mail($recipient, $subject, $formcontent, $mailheader)) {
 				    $message = "Your contact information is received successfully.";
 				    $type = "success";
 				}
-			}
+				else echo '<p> There is an error. Please try again.</p>';
+			} 
 			require_once "contact.php";
 				
 			echo "Thank You!" . " -" . "<a href='Contact.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
